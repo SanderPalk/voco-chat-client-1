4494cf052a9c4b2dbee029bc81c21312a43b18fb
kunter@bitweb.ee <kunterpartel@gmail.com>
2022-01-31 18:15:32 +0200

    Inital

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Chat from "./Components/Chat";
+
+const App:React.FC = () => {
+  return (
+   <div>
+     <Chat />
+   </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Chat.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../App.css';
+
+function Chat() {
+    return (
+        <div className="chat">
+            <h3>KHK</h3>
+
+            <div className="chat__container">
+<h3>woot</h3>
+
+            </div>
+        </div>
+    );
+}
+
+export default Chat;
