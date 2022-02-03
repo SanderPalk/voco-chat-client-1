982661e1f4dbced0f89b84f6d2a647335d305585
Künter Pärtel <kunterpartel@gmail.com>
2022-02-03 21:23:09 +0200

    Mocked data

--- src/Components/Chat.tsx
+++ src/Components/Chat.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import '../App.css';
+import data from '../data.json';
+import ChatCard from './ChatCard';
 
-function Chat() {
+const Chat: React.FC = () => {
     return (
         <div className="chat">
             <h3>KHK</h3>
 
             <div className="chat__container">
-<h3>woot</h3>
+                {data.map((cardItem) => {
+                    return (
+                        <ChatCard key={cardItem.id} username={cardItem.username} message={cardItem.message} date={cardItem.date} />
+                    )
+                })}
 
             </div>
         </div>
